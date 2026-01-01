34720e8669dfd46b8455c4d9cf3c127675e2ad9a
361dara-dev <361dara@gmail.com>
2026-01-01 16:10:34 +0000

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,14 +1,31 @@
 let fixtures = [];
+let currentFixtureIndex = 0;
 
-function regenerateSchedule() {
+/* =========================
+   GENERATE DOUBLE ROUND ROBIN
+========================= */
+
+function regenerateSchedule(season) {
   fixtures = [];
+  currentFixtureIndex = 0;
+
   for (let i = 0; i < teams.length; i++) {
     for (let j = i + 1; j < teams.length; j++) {
-      fixtures.push({ home: teams[i], away: teams[j] });
-      fixtures.push({ home: teams[j], away: teams[i] });
+      fixtures.push({
+        home: teams[i].name,
+        away: teams[j].name,
+        played: false,
+        season
+      });
+
+      fixtures.push({
+        home: teams[j].name,
+        away: teams[i].name,
+        played: false,
+        season
+      });
     }
   }
 }
-function saveGame() {
-  localStorage.setItem("pslGame", JSON.stringify(gameState));
-}
+
+
